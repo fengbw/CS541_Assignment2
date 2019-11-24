b2da4be62ababb81fd94490986064a23dc00f92b
fengbw <fengbwseu@163.com>
2019-11-24 16:33:45 -0500

    [add] graph method

--- dataReader.py
+++ dataReader.py
@@ -2,6 +2,7 @@ def dataReader(filename):
     dataset = []
     with open(filename, 'r') as reader:
         for i, line in enumerate(reader.readlines()):
+            print("this is line :", i)
             row = line.strip().split(' ')
             row = list(map(int, row))
             dataset.append(row[1:])

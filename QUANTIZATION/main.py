74e0d5c1e501187a9a1b1789d7e8ba1d3996745c
congdeng <1829929@qq.com>
2019-11-24 16:03:12 -0500

    [update] naopq

--- QUANTIZATION/main.py
+++ QUANTIZATION/main.py
@@ -1,45 +1,31 @@
 import nanopq
 import numpy as np
 import time
-import pandas as pd
+from dataReader import dataReader
 
 if __name__ == "__main__":
-    number_of_queries = 1000
-    number_of_tables = 50
-
-    dataset_file = '../game_dataset.csv'
-    df_data = pd.read_csv(dataset_file, index_col=0)
-    df_data = df_data.astype(np.float32)
-    df_data = df_data[:9999]
-    np_data = df_data[['x', 'y']].values
-
-    # falconn requires use float32
-    assert np_data.dtype == np.float32
-
-    # using the cosine similarity, normalize data
-    np_data /= np.linalg.norm(np_data, axis=1).reshape(-1, 1)
-
-    queries = np.array(np_data[len(np_data) - number_of_queries:])
-    dataset = np.array(np_data[:len(np_data) - number_of_queries])
+    data = np.array(dataReader("../P53_test.ds"),dtype = np.float32)
+    print(np.shape(data))
+    dim = 5408
     # N, Nt, D = 10000, 2000, 128
     # X = np.random.random((N, D)).astype(np.float32)  # 10,000 128-dim vectors to be indexed
     # Xt = np.random.random((Nt, D)).astype(np.float32)  # 2,000 128-dim vectors for training
-    # query = np.random.random((D,)).astype(np.float32)  # a 128-dim query vector
+    query = np.random.random((dim,)).astype(np.float32)  # a query vector
 
     # Instantiate with M=8 sub-spaces
-    pq = nanopq.PQ(M=2)
+    pq = nanopq.PQ(M=8)
 
     t1 = time.time()
     # Train codewords
-    pq.fit(np_data)
+    pq.fit(data,seed = 1000)
     print(time.time()-t1)
 
     t2 = time.time()
     # Encode to PQ-codes
-    X_code = pq.encode(np_data)  # (10000, 8) with dtype=np.uint8
+    X_code = pq.encode(data)  # (10000, 8) with dtype=np.uint8
     print("Encode time is ", time.time()-t2)
 
     # Results: create a distance table online, and compute Asymmetric Distance to each PQ-code
     t3 = time.time()
-    dists = pq.dtable(queries[0]).adist(X_code)  # (10000, )
+    dists = pq.dtable(query).adist(X_code)  # (10000, )
     print("query time is ",time.time()-t3)

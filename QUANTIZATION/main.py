bdff91f7ab739f3886b8c8119e5713e736de36e4
congdeng <1829929@qq.com>
2019-11-25 22:15:43 -0500

    [update] nanopq

--- QUANTIZATION/main.py
+++ QUANTIZATION/main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     t2 = time.time()
     # Encode to PQ-codes
-    X_code = pq.encode(data[::])  # (10000, 8) with dtype=np.uint8
+    X_code = pq.encode(data[::])
     print("Encode time is ", time.time()-t2)
     # Results: create a distance table online, and compute Asymmetric Distance to each PQ-code
     t3 = time.time()
@@ -30,13 +30,13 @@ if __name__ == "__main__":
     score = 0
     for i in range(np.shape(test)[0]):
         res = []
-        dists = pq.dtable(test[i]).adist(X_code)  # (10000, )
+        dists = pq.dtable(test[i]).adist(X_code)
         res.append(list(np.argsort(dists)[:5]))
         if res[0][1] == right[i][1]:
             score += 1
-        sys.stdout.write("\r testing #{}, now score is {}".format(i,score))
+        sys.stdout.write("\rTesting #{}, now score is {}".format(i,score))
     print()
-    print("accuracy is ", score/3000)
+    print("accuracy is ", score/np.shape(test)[0])
     print("query time is ", (time.time()-t3)/np.shape(test)[0])
 
     # # exact scan

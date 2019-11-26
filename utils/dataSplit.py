41ce618bcc680334cd7566d1ccd77ee8c0eaacf1
fengbw <fengbwseu@163.com>
2019-11-25 22:00:47 -0500

    [add] grapg

--- utils/dataSplit.py
+++ utils/dataSplit.py
@@ -7,9 +7,6 @@ def data_split():
             if i >= 3000:
                 with open('dataset/P53_train.ds', 'a') as writer:
                     writer.write(line)
-#         row = line.strip().split(' ')
-#         row = list(map(int, row))
-#         dataset.append(row[1:])
-# print(len(dataset[0]))
+                    
 if __name__ == "__main__":
     data_split()

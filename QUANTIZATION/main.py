463535b420dec9b4518485b059f069c0d927a889
ciuji's mbp <zuochaoji@gmail.com>
2019-11-29 15:25:20 -0500

    [update] report

--- QUANTIZATION/main.py
+++ QUANTIZATION/main.py
@@ -2,21 +2,26 @@ import nanopq
 import numpy as np
 import time
 import sys
-from dataReader import *
+import os
+sys.path.append(os.getcwd())
+
+from utils import *
+#from dataReader import *
 
 if __name__ == "__main__":
     # data = np.array(dataReader("../P53_train.ds"),dtype = np.float32)
-    data = np.load("train.npy")
+    data = np.load("dataset/trainset/raw_train.npy")[:28000]
     # test = np.array(dataReader("../P53_test.ds"), dtype=np.float32)
-    test = np.load("test.npy")
+    test = np.load("dataset/testset/raw_test.npy")[:1500]
     dim = 5408
-
+    data/= np.linalg.norm(data,axis=1).reshape(-1,1)
+    test/= np.linalg.norm(test,axis=1).reshape(-1,1)
     # Instantiate with M=8 sub-spaces
-    pq = nanopq.PQ(M=676)
+    pq = nanopq.OPQ(M=208,Ks=512,verbose=False)
 
     # Train codewords
     t1 = time.time()
-    pq.fit(vecs=data[:8000], seed= 100)
+    pq.fit(vecs=data[:6000], seed= 100,)
     print(time.time()-t1)
 
     t2 = time.time()
@@ -24,20 +29,18 @@ if __name__ == "__main__":
     X_code = pq.encode(data[::])
     print("Encode time is ", time.time()-t2)
     # Results: create a distance table online, and compute Asymmetric Distance to each PQ-code
+    right = np.load("groundtruth/linearScanResult28000.npy")[:len(test)]
+
     t3 = time.time()
-    right = np.load("../groundtruth/linearScanResult3000.npy")
+    #right = np.load("groundtruth/linearScanResultEuclidean2800_28000.npy")
+
     res = []
-    score = 0
     for i in range(np.shape(test)[0]):
-        res = []
         dists = pq.dtable(test[i]).adist(X_code)
         res.append(list(np.argsort(dists)[:5]))
-        if res[0][1] == right[i][1]:
-            score += 1
-        sys.stdout.write("\rTesting #{}, now score is {}".format(i,score))
-    print()
-    print("accuracy is ", score/np.shape(test)[0])
     print("query time is ", (time.time()-t3)/np.shape(test)[0])
+
+    print("accuracy is ",compareResult(right,res))
 
     # # exact scan
     # distsExact = np.linalg.norm(data - query, axis=1) ** 2
